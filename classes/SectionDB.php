efdf2c7faf89876b365d1de59a45f6ca8c6fb379
Marcin Cekiera <marcin.cekiera@gmail.com>
2017-05-21 16:38:20 +0200

    working cms, escaping char problem fixed all

--- classes/SectionDB.php
+++ classes/SectionDB.php
@@ -19,12 +19,24 @@ class SectionDB
 
 
     function insert($name, $title, $domid, $menu, $content) {
-        $sql = "INSERT INTO section VALUES (null, '{$name}', '{$title}', '{$domid}', '{$menu}', '{$content}')";
+        $n = $this->mysql->escape($name);
+        $t = $this->mysql->escape($title);
+        $d = $this->mysql->escape($domid);
+        $m = $this->mysql->escape($menu);
+        $c = $this->mysql->escape($content);
+
+        $sql = "INSERT INTO section VALUES (null, '{$n}', '{$t}', '{$d}', '{$m}', '{$c}')";
         return $this->mysql->query($sql);
     }
 
     function update($id, $name, $title, $domid, $menu, $content) {
-        $sql = "UPDATE section SET name='{$name}', title='{$title}', domid='{$domid}', menu='{$menu}', content='{$content}' WHERE id={$id}";
+        $n = $this->mysql->escape($name);
+        $t = $this->mysql->escape($title);
+        $d = $this->mysql->escape($domid);
+        $m = $this->mysql->escape($menu);
+        $c = $this->mysql->escape($content);
+
+        $sql = "UPDATE section SET name='{$n}', title='{$t}', domid='{$d}', menu='{$m}', content='{$c}' WHERE id={$id}";
         $this->mysql->query($sql);
         return $sql;
     }

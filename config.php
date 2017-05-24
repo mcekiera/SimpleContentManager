2f0ae8384749aa978b7aac5a518d8a408be0ac06
Marcin Cekiera <marcin.cekiera@gmail.com>
2017-05-25 00:43:05 +0200

    fixes for deployed version lesser issues

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 
 $config = array(
     "db" => array(
-        "name" => "mcekiera_cahee",
-        "user" => "mcekiera_admin",
-        "password" => "1a2b3x",
-        "host" => "localhost"
+        "name" => "***",
+        "user" => "***",
+        "password" => "***",
+        "host" => "***"
     ),
     "urls" => array(
         "baseUrl" => ""
